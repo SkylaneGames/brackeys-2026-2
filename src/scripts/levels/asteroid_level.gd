a5a2725d9fae5e5219d70051e96d4dc8ec546c01
Harvey <harvey_west@live.com>
2026-08-25 20:59:08 +0100

    Basic asteroids

--- src/scripts/levels/asteroid_level.gd
+++ src/scripts/levels/asteroid_level.gd
@@ -1,7 +1,18 @@
 extends Level
 
+const LANE_OFFSETS: Array[int] = [-1, 0, 1]
+
+@export var asteroid_scene: PackedScene
+@export_range(5, 7) var lane_count := 6
+@export_range(1.0, 1000.0) var asteroid_fall_speed := 180.0
+@export_range(80.0, 400.0) var row_spacing := 150.0
+
 @onready var lives_label: Label = %LivesLabel
 @onready var pause_menu: Control = %PauseMenu
+@onready var spawn_timer: Timer = %AsteroidSpawnTimer
+@onready var asteroids: Node2D = $Asteroids
+
+var safe_lane := 0
 
 
 func _ready() -> void:
@@ -9,6 +20,28 @@ func _ready() -> void:
 	player.lives_changed.connect(_on_lives_changed)
 	_on_lives_changed(player.lives)
 	GameManager.state_changed.connect(_on_game_state_changed)
+	safe_lane = floori(lane_count / 2.0)
+	spawn_timer.wait_time = row_spacing / asteroid_fall_speed
+	spawn_timer.timeout.connect(_spawn_asteroid_row)
+	spawn_timer.start()
+	_spawn_asteroid_row()
+
+
+func _spawn_asteroid_row() -> void:
+	if asteroid_scene == null:
+		push_error("Asteroid Level requires an asteroid scene.")
+		return
+	var lane_offset: int = LANE_OFFSETS.pick_random()
+	safe_lane = clampi(safe_lane + lane_offset, 0, lane_count - 1)
+	var lane_width := 1280.0 / lane_count
+	for lane in lane_count:
+		if lane == safe_lane:
+			continue
+		var asteroid := asteroid_scene.instantiate() as Asteroid
+		asteroid.position = Vector2((lane + 0.5) * lane_width, -50.0)
+		asteroid.scale.x = (lane_width - 12.0) / 112.0
+		asteroid.fall_speed = asteroid_fall_speed
+		asteroids.add_child(asteroid)
 
 
 func _on_lives_changed(lives: int) -> void:
