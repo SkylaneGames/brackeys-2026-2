bfb42b3a562743993ed94363929d3c0a08c557a0
Jeremy Sapsed <dev@jsapsed.co.uk>
2026-08-29 12:43:49 +0100

    Add the additional player roll animations

--- src/scripts/player/player.gd
+++ src/scripts/player/player.gd
@@ -9,6 +9,7 @@ signal died
 @export var invulnerability_duration := 1.5
 
 @export var explosion_template: PackedScene
+@onready var anim_player: AnimatedSprite2D = $AnimatedSprite2D
 
 var lives: int
 var invulnerability_left := 0.0
@@ -17,6 +18,8 @@ var invulnerability_left := 0.0
 func _ready() -> void:
 	lives = starting_lives
 	lives_changed.emit(lives)
+	if not anim_player.animation_finished.is_connected(_on_animation_finished):
+		anim_player.animation_finished.connect(_on_animation_finished)
 
 
 func _physics_process(delta: float) -> void:
@@ -25,6 +28,7 @@ func _physics_process(delta: float) -> void:
 	velocity = Vector2(direction * move_speed, 0.0)
 	move_and_slide()
 	global_position.x = clampf(global_position.x, 32.0, 1248.0)
+	_update_animation(direction)
 
 
 func take_hit() -> void:
@@ -48,3 +52,34 @@ func _update_invulnerability(delta: float) -> void:
 	modulate.a = 0.35 if int(invulnerability_left * 10.0) % 2 == 0 else 1.0
 	if invulnerability_left == 0.0:
 		modulate.a = 1.0
+
+
+func _update_animation(direction: float) -> void:
+	if direction < 0.0:
+		if anim_player.animation != "side_left" and (anim_player.animation != "roll_left" or anim_player.get_playing_speed() <= 0.0):
+			anim_player.play("roll_left")
+	elif direction > 0.0:
+		if anim_player.animation != "side_right" and (anim_player.animation != "roll_right" or anim_player.get_playing_speed() <= 0.0):
+			anim_player.play("roll_right")
+	else:
+		if anim_player.animation == "side_left" or (anim_player.animation == "roll_left" and anim_player.get_playing_speed() > 0.0):
+			anim_player.play_backwards("roll_left")
+		elif anim_player.animation == "side_right" or (anim_player.animation == "roll_right" and anim_player.get_playing_speed() > 0.0):
+			anim_player.play_backwards("roll_right")
+		elif anim_player.animation == "roll_left" or anim_player.animation == "roll_right":
+			pass
+		elif anim_player.animation != "default":
+			anim_player.play("default")
+
+
+func _on_animation_finished() -> void:
+	if anim_player.animation == "roll_left":
+		if anim_player.frame == 0:
+			anim_player.play("default")
+		else:
+			anim_player.play("side_left")
+	elif anim_player.animation == "roll_right":
+		if anim_player.frame == 0:
+			anim_player.play("default")
+		else:
+			anim_player.play("side_right")
